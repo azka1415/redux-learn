4a8f3fca7ab4c7fa68c0ea1fe6d295fbd872445d
azka1415 <rafifzaydan4@gmail.com>
2023-02-10 15:07:46 +0700

    :sparkles: added tailwindcss
    added and changed classes to tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
-import "./App.css";
 import { Counter } from "./features/counter/Counter";
 
 function App() {
 	return (
-		<div className="App">
-			<div>
-				<Counter />
-			</div>
+		<div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center">
+			<div className="bg-yellow-500 w-[100vw] h-[60vh] absolute z-0 inset-0"></div>
+			<Counter />
 		</div>
 	);
 }
--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -7,20 +7,24 @@ export function Counter() {
 	const dispatch = useDispatch();
 
 	return (
-		<div>
-			<div>
-				<button
-					aria-label="Increment value"
-					onClick={() => dispatch(increment())}
-				>
-					Increment
-				</button>
-				<span>{count}</span>
+		<div className="z-10 flex flex-col text-center w-[50rem] bg-pink-400 h-[30rem] rounded-2xl p-2 shadow-2xl border-none outline-none">
+			<div className="text-5xl flex flex-col font-bold h-full justify-center items-center">
+				<h1>{count}</h1>
+			</div>
+			<div className="h-20 flex flex-row justify-between items-center p-2 font-semibold">
 				<button
 					aria-label="Decrement value"
 					onClick={() => dispatch(decrement())}
+					className="action-button"
 				>
 					Decrement
+				</button>
+				<button
+					aria-label="Increment value"
+					onClick={() => dispatch(increment())}
+					className="action-button"
+				>
+					Increment
 				</button>
 			</div>
 		</div>
